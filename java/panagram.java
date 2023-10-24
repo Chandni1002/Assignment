7effac79da4acc7ddd33e27cf7177f1b5598f314
Chandni1002 <96787036+Chandni1002@users.noreply.github.com>
2023-10-24 22:34:44 +0530

    Update panagram.java

--- java/panagram.java
+++ java/panagram.java
@@ -1,14 +1,23 @@
-String str=sc.nextLine();
-		str=str.replaceAll("","").toLowerCase();
-		String res="";
-    for(char i='a';i<='z';i++){
-			if(str.indexOf(i)!=-1){
-				res=res+i;
-			}
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String str = sc.nextLine();
+        str = str.replaceAll("[^a-zA-Z]", "").toLowerCase();
+        String res = "";
+        
+        for (char i = 'a'; i <= 'z'; i++) {
+            if (str.indexOf(i) != -1) {
+                res = res + i;
+            }
+        }
+        
+        if (res.length() == 26) {
+            System.out.println("Pangram");
+        } else {
+            System.out.println("Not Pangram");
+        }
     }
-		if(res.length()==26){
-			System.out.println("Pangram");
-		}
-		else{
-			System.out.println(" Not Pangram");
-		}
+}
