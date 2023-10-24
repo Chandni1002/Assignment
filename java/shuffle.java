f6e51b69d4aadc71c59ab051a92f6ee6561b1e4a
Chandni1002 <96787036+Chandni1002@users.noreply.github.com>
2023-10-24 22:37:09 +0530

    Update shuffle.java

--- java/shuffle.java
+++ java/shuffle.java
@@ -1,12 +1,23 @@
-private static void shuffleAnArray(int[] array)
-{
-    int index, temp;
-    Random random = new Random();
-    for (int i = array.length - 1; i > 0; i--)
-    {
-        index = random.nextInt(i + 1);
-        temp = array[index];
-        array[index] = array[i];
-        array[i] = temp;
+import java.util.Arrays;
+import java.util.Random;
+
+public class ArrayShuffler {
+
+    private static void shuffleAnArray(int[] array) {
+        int index, temp;
+        Random random = new Random();
+        for (int i = array.length - 1; i > 0; i--) {
+            index = random.nextInt(i + 1);
+            temp = array[index];
+            array[index] = array[i];
+            array[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5}; 
+        System.out.println("Original Array: " + Arrays.toString(numbers));
+        shuffleAnArray(numbers);
+        System.out.println("Shuffled Array: " + Arrays.toString(numbers));
     }
 }
