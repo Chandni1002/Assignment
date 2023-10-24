9549527f2c81fdae565d1e8c7f63108c29070429
Chandni1002 <96787036+Chandni1002@users.noreply.github.com>
2023-10-24 22:32:03 +0530

    Update sortDes.js

--- JavaScript/sortDes.js
+++ JavaScript/sortDes.js
@@ -1,5 +1,16 @@
-function arrSort(arr) { 
-    arr.sort((a,b)=>a-b); 
-    arr.reverse(); 
-    return arr; 
+function arrSort(arr) {
+    arr.sort((a, b) => a - b);
+    arr.reverse();
+    return arr;
 }
+
+function main() {
+    let numbers = [1,2,3,4,5,6,7];
+    console.log("Original Array: " + numbers.join(", "));
+    
+    let sortedArray = arrSort(numbers);
+    console.log("Sorted and Reversed Array: " + sortedArray.join(", "));
+}
+
+main(); 
+
