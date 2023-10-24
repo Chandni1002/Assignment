95b1329fe2906a05dd0362622901bca369faca7e
Chandni1002 <96787036+Chandni1002@users.noreply.github.com>
2023-10-24 22:35:48 +0530

    Update romanToInt.java

--- java/romanToInt.java
+++ java/romanToInt.java
@@ -1,46 +1,49 @@
+public class RomanToIntConverter {
 
-  int value(char ch)   
-  {   
-    if (ch == 'I')   
-      return 1;   
-    if (ch== 'V')   
-      return 5;   
-    if (ch == 'X')   
-      return 10;   
-    if (ch == 'L')   
-      return 50;   
-    if (ch == 'C')   
-      return 100;   
-    if (ch == 'D')   
-      return 500;   
-    if (ch == 'M')   
-      return 1000;   
-    return -1;   
-  }   
+    public int romanToInt(String str) {
+        int res = 0;
+        for (int i = 0; i < str.length(); i++) {
+            int s1 = value(str.charAt(i));
+            if (i + 1 < str.length()) {
+                int s2 = value(str.charAt(i + 1));
+                if (s1 >= s2) {
+                    res = res + s1;
+                } else {
+                    res = res - s1;
+                }
+            } else {
+                res = res + s1;
+            }
+        }
+        return res;
+    }
 
+    private int value(char ch) {
+        switch (ch) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return -1;
+        }
+    }
 
-  int RomanToInti(String str)   
-  {     
-    int res = 0;    
-    for (int i=0; i<str.length(); i++)   
-    {   
-      int s1 = value(str.charAt(i));    
-      if (i+1 <str.length())   
-      {   
-        int s2 = value(str.charAt(i+1));     
-        if (s1 >= s2)   
-        {   
-          res = res + s1;   
-        }   
-        else  
-        {   
-          res = res - s1;   
-        }   
-      }   
-      else  
-      {   
-        res = res + s1;   
-      }  
-  }
-return res;
+    public static void main(String[] args) {
+        RomanToIntConverter converter = new RomanToIntConverter();
+        String romanNumeral = "XIV"; 
+        int intValue = converter.romanToInt(romanNumeral);
+        System.out.println("Roman Numeral: " + romanNumeral);
+        System.out.println("Integer Value: " + intValue);
+    }
 }
